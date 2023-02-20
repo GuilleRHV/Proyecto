43fb8a8bab1c90c1e7e1ecbc603f4756bcca4953
guillermo rodriguez <guillerhuguet@gmail.com>
2023-02-20 16:18:49 +0000

    av

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,16 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Attribute;
 class Product extends Model
 {
     use HasFactory;
 
 
+
     protected $fillable = ['nombre', 'descripcion', 'precio'];
 
 
-
+    protected function Nombre(): Attribute
+    {
+        return new Attribute(
+            fn ($value) => strtoupper($value),
+            fn ($value) => ucfirst(strtolower($value)),
+        );
+    }
 
     //mutator -> set<nombre_atributo>Attribute -> direccion:: hacia la bbdd
     //accesor -> get
@@ -31,6 +38,6 @@ class Product extends Model
 
     public function getPrecioAttribute($value)
     {
-        return ($value ." euros"); //Devuelve el nombre en mayusculas
+        return ($value . " euros"); //Devuelve el nombre en mayusculas
     }
 }
